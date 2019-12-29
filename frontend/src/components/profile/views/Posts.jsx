1a674d4eb29b3513ef35838985c1a01bd94951cf
Gustavo Rivera Pecunia <grpecunia@gmail.com>
2019-12-28 23:53:13 -0500

    changes to Posts metrics

--- frontend/src/components/profile/views/Posts.jsx
+++ frontend/src/components/profile/views/Posts.jsx
@@ -7,7 +7,6 @@ import actions from "../../../services/index";
 
 
 class Posts extends Component {
-
   state = {};
 
   async componentDidMount() {
@@ -67,33 +66,133 @@ class Posts extends Component {
       });
   }
 
+  renderDate = (postDate) =>{
+            let a = new Date(postDate * 1000);
+            let year = Number(a.getFullYear().toString().slice(2, 4));
+            let month = a.getMonth() + 1;
+            let date = a.getDate();
+            let time = month + "/" + date + "/" + year;
+            return String(time);
+  }
+  
+
+  postList() {
+    return this.state.posts.map((posts, i) => {
+      return (
+        <Col lg={3} md={4} sm={6} xs={12} className="font-icon-list" key={i}>
+          <div style={{ marginBottom: "2rem" }}>
+            <div
+              className="g-card m-2"
+              style={{ width: "13rem", height: "27rem" }}
+            >
+              {/* For full width Do 60rem for width above and 30rem for height on g card */}
+              <div className="card-container">
+                <div className="card-front  d-flex flex-column justify-content-between">
+                  <img
+                    className="card-img-top img-fluid"
+                    alt={posts.owner_id}
+                    // style={{ width: "100%" }}
+                    src={posts.thumbnail}
+                  />
+                  <div
+                    className="card-body d-flex align-items-center justify-content-center"
+                    style={{ background: "white" }}
+                  >
+                    <p className="card-text">
+                      <h5>
+                        <strong>Post eML Rate: </strong>{" "}
+                        {(
+                          ((Number(posts.like_count) +
+                            Number(posts.comment_count)) /
+                            Number(this.state.profile.edge_followed_by.count)) *
+                          100
+                        ).toFixed(2)}
+                        %
+                      </h5>
+                      <h6>
+                        <strong>Like Count:</strong>{" "}
+                        {posts.like_count.toLocaleString(navigator.language, {
+                          minimumFractionDigits: 0
+                        })}
+                        <br />
+                        <strong>Comment Count: </strong>{" "}
+                        {posts.comment_count.toLocaleString(
+                          navigator.language,
+                          { minimumFractionDigits: 0 }
+                        )}
+                      </h6>
+                    </p>
+                  </div>
+                </div>
+                <div className="card-back d-flex flex-row justify-content-center">
+                  <div className="backContent">
+                    <div className="card-text backTitle">
+                      <strong>Posted Date</strong><br/>
+                      {this.renderDate(posts.date)}
+                    </div>
+                    <br></br>
+                    <p>{posts.text}</p>
+                    <div className="card-text backInfo">
+                      <strong>Likes:</strong>{" "}
+                      {posts.like_count.toLocaleString(navigator.language, {
+                        minimumFractionDigits: 0
+                      })}
+                      <br />
+                      <br />
+                      <p>
+                        {" "}
+                        <strong>Comments: </strong>{" "}
+                        {posts.comment_count.toLocaleString(
+                          navigator.language,
+                          { minimumFractionDigits: 0 }
+                        )}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Col>
+      );
+    });
+  }
 
   render() {
-    return (
-      <div className="content">
-        <Container fluid="true">
-          <Row>
-            <Col md={12}>
-              <Card
-                hCenter
-                title="Your Content - Track your Strategy"
-                category="Post-Related Metrics - COMING SOON!"
-                ctTableResponsive
-                content={
-                  <div style={{ textAlign: "center" }}>
-                    
-                    
-                    <br />
-                    <img src={logo} alt="EngagementML" width="400" />
-                    <br />
-                  </div>
-                }
-              />
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
+    if (this.state.profile !== undefined && this.state.posts !== undefined) {
+      return (
+        <div className="content">
+          <Container fluid="true">
+            <Row>
+              <Col md={12}>
+                <Card
+                  title={
+                    <React.Fragment>
+                      <img
+                        src={logo}
+                        alt='EngagementML'
+                        width='400'
+                      />
+                    </React.Fragment>
+                  }
+                  ctAllIcons
+                  category="Additional Metrics Coming Soon!"
+                  content={<Row>{this.postList()}</Row>}
+                />
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h3 className="loading" style={{ textAlign: "center" }}>
+            Loading...
+          </h3>
+        </div>
+      );
+    }
   }
 }
 
