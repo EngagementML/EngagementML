7876a62b8f55d676072ddfe52c0c8ce4da3183f5
ChabaJosa <chabagjg@gmail.com>
2020-03-09 15:10:29 -0400

    Second Commit

---
+++ random.js
@@ -0,0 +1,5 @@
+var ig = require("instagram-scraping");
+ig.scrapeUserPage('rctv').then(result => {
+    console.dir(result);
+    console.log('These are the followers',result.user.edge_followed_by.count)
+});
