71b4e60bed018f2e2b76c5ba86179fac9252a2b7
ChabaJosa <chabagjg@gmail.com>
2020-03-09 18:28:34 -0400

    Added deployment

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000", "https://distracted-noyce-fee0e2.netlify.com"] //Swap this with the client url 
+    origin: ["http://localhost:3000", "https://ecstatic-davinci-001bc3.netlify.com"] //Swap this with the client url 
   })
 );
 
