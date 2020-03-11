b750470d26e24116aed7d0ba864ac373740dee00
Gustavo Rivera Pecunia <grpecunia@gmail.com>
2020-03-11 17:12:32 -0400

    changes to the padding in the home page

--- frontend/src/components/auth/LogIn.js
+++ frontend/src/components/auth/LogIn.js
@@ -3,6 +3,7 @@ import actions from '../../services/index'
 import logo from '../../images/engagementML.png'
 import { Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
+import { FaKey, FaUser } from "react-icons/fa";
 
 class LogIn extends Component {
   state = {};
@@ -38,7 +39,7 @@ class LogIn extends Component {
                   <div className="input-group mb-3">
                     <div className="input-group-append">
                       <span className="input-group-text">
-                        <i className="fas fa-user" />
+                        <FaUser />
                       </span>
                     </div>
                     <input
@@ -50,7 +51,7 @@ class LogIn extends Component {
                   <div className="input-group mb-2">
                     <div className="input-group-append">
                       <span className="input-group-text">
-                        <i className="fas fa-key" />
+                        <FaKey />
                       </span>
                     </div>
                     <input
