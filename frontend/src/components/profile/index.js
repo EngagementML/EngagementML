ae94cbe1277da63aca4ed6b621237c1f731d0080
Gustavo Rivera Pecunia <grpecunia@gmail.com>
2020-03-13 15:53:25 -0400

    grooving back

--- frontend/src/components/profile/index.js
+++ frontend/src/components/profile/index.js
@@ -22,7 +22,8 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/animate.min.css";
-import "./assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0";
+// import "./assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0";
+
 import "./assets/css/demo.css";
 import "./assets/css/pe-icon-7-stroke.css";
 
