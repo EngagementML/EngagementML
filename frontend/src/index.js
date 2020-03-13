8b9efc81ad447f00c18d251e2879dbbc88673486
ChabaJosa <chabagjg@gmail.com>
2020-03-13 11:29:41 -0400

    Added font-awesome

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "bootstrap/dist/css/bootstrap.css"
 import "./main.css";
 import "./noscript.css";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import "bootstrap/dist/css/bootstrap.css"
+import "font-awesome/css/font-awesome.min.css";
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
