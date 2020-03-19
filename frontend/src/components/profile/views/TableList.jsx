435bed86b8b11107acb4835482ab7758adf3e9ea
ChabaJosa <chabagjg@gmail.com>
2020-03-19 01:02:09 -0400

    Added trending carousel

--- frontend/src/components/profile/views/TableList.jsx
+++ frontend/src/components/profile/views/TableList.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container, Row, Col, Table, Form, Button } from "react-bootstrap";
+import { Container, Row, Col, Table, Form, Button, Carousel, Header } from "react-bootstrap";
 import axios from "axios";
 import Card from "../components/Card/Card.jsx";
 import { thArray } from "../variables/Variables.jsx";
@@ -10,15 +10,31 @@ state = {tag:"Awesome"}
   
 componentDidMount(){
   this.getData()
+
+  const HashtagCallTrending = `https://cors-anywhere.herokuapp.com/https://api.ritekit.com/v1/search/trending?latin=1&client_id=0c6df3574f5c1c81c1541d575b506bcbcd261454eca9`
+    axios.get(HashtagCallTrending).then(result => {
+    console.log("Full Result Trending", result)
+    this.setState({ hashtagResultTrending: result.data })
+  })
+    .catch((err) => console.log("Can’t access " + HashtagCallTrending, err))
 }
 
 getData() {
-  const HashtagCall = `https://cors-anywhere.herokuapp.com/https://api.ritekit.com/v1/stats/hashtag-suggestions?text=${this.state.tag}&client_id=0c6df3574f5c1c81c1541d575b506bcbcd261454eca9`
-    axios.get(HashtagCall).then(result => {
-    console.log("Full Result", result)
-    this.setState({ hashtagResult: result.data })
+  const HashtagCallWord = `https://cors-anywhere.herokuapp.com/https://api.ritekit.com/v1/stats/hashtag-suggestions?text=${this.state.tag}&client_id=0c6df3574f5c1c81c1541d575b506bcbcd261454eca9`
+    axios.get(HashtagCallWord).then(result => {
+    console.log("Full Result Word", result)
+    this.setState({ hashtagResultWord: result.data })
   })
-    .catch((err) => console.log("Can’t access " + HashtagCall, err))
+    .catch((err) => console.log("Can’t access " + HashtagCallWord, err))
+
+  // There's something wrong with the history api link
+
+  // const HashtagCallWordHistory = `https://cors-anywhere.herokuapp.com/https://api.ritekit.com/v1/stats/history/${this.state.tag}&client_id=0c6df3574f5c1c81c1541d575b506bcbcd261454eca9`
+  //   axios.get(HashtagCallWordHistory).then(result => {
+  //   console.log("Full Result Word History", result)
+  //   this.setState({ hashtagResultWordHistory: result.data })
+  // })
+  //   .catch((err) => console.log("Can’t access " + HashtagCallWordHistory, err))
 }
 
 changeTag = async (e) => {
@@ -33,10 +49,23 @@ updateTag = (e) =>{
 }
 
   render() {
-    if (this.state.hashtagResult !== undefined && this.state.tag !== 'undefined') {
+    if (this.state.hashtagResultWord !== undefined && this.state.hashtagResultTrending !== undefined &&  this.state.tag !== 'undefined') {
     console.log("Current State at Render", this.state)
     return (
       <div className="content">
+      <h1 className="display-1">#Trending</h1>
+      <Carousel>
+      {this.state.hashtagResultTrending.tags.map((currentTag) => {
+          return (
+          <Carousel.Item>
+            <h1 style={{color:"black",textAlign:"center"}} >{"#"+currentTag.tag}</h1>
+            <h5 style={{color:"black",textAlign:"center"}} className="mb-5">{currentTag.exposure.toLocaleString(navigator.language, { minimumFractionDigits: 0 })+ " Impressions"}</h5>
+          </Carousel.Item>
+          )
+          })
+        }
+      </Carousel>
+
         <Container fluid="true">
           {/* This below works like Gustavo's ticker search bar but we'll run out of api calls */}
           {/* <Form.Group >
@@ -45,7 +74,7 @@ updateTag = (e) =>{
             </Form.Group> */}
           <Form onSubmit={this.updateTag} >
           <Form.Group className="d-flex flex-row align-items-center"role="form">
-              <Form.Control onChange={this.changeTag}  name="tagChanger" size="lg" type="text" placeholder="#Anything" />
+              <Form.Control onChange={this.changeTag}  name="tagChanger" size="lg" type="text" placeholder="#SearchForAnything" />
               <br />
               <Button className="ml-2" variant="info" type="submit" style={{padding:"0.5rem", height:"5%"}}>#</Button>
           </Form.Group>
@@ -63,20 +92,24 @@ updateTag = (e) =>{
                     <thead>
                       <tr>
                         {thArray.map((prop, key) => {
-                          return <th key={key}>{prop}</th>;
+                          if (prop==="Hashtag"){
+                            return <th key={key} >{prop}</th>;
+                          }else {
+                            return <th key={key} style={{textAlign:"right"}}>{prop}</th>;
+                          }
                         })}
                       </tr>
                     </thead>
                     <tbody>
 
-                      {this.state.hashtagResult.data.map((currentTag) => {
+                      {this.state.hashtagResultWord.data.map((currentTag) => {
                         return (
                           <tr>
                             <td>{"#"+currentTag.hashtag}</td>
-                            <td>{currentTag.exposure}</td>
-                            <td>{currentTag.images}</td>
-                            <td>{currentTag.links}</td>
-                            <td>{currentTag.mentions}</td>
+                            <td style={{textAlign:"right"}}>{currentTag.exposure.toLocaleString(navigator.language, { minimumFractionDigits: 0 })}</td>
+                            <td style={{textAlign:"right"}}>{currentTag.images}</td>
+                            <td style={{textAlign:"right"}}>{currentTag.links}</td>
+                            <td style={{textAlign:"right"}}>{currentTag.mentions}</td>
                           </tr>
                         )
                         })
