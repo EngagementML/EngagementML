e369f24356c3d7b7999123830d52acfa470178b9
Gustavo Rivera Pecunia <grpecunia@gmail.com>
2020-03-19 07:32:41 -0400

    changes in route app.js

--- backend/app.js
+++ backend/app.js
@@ -106,7 +106,7 @@ app.route("/profiles").get((req, res, next) => {
 app.route("/profile/:username").get((req, res) => {
   let username = req.params.username;
   InstaProfile.findOne({username : username}, { full_name:1, biography :1, username: 1, profile_pic_url_hd
-: 1, "edge_follow.count" : 1, "edge_followed_by.count" : 1, _id:0}, (err, instaprofiles) => {
+: 1, edge_follow : 1, edge_followed_by : 1, _id:0}, (err, instaprofiles) => {
     if (err) {
       console.log(err);
     } else {
