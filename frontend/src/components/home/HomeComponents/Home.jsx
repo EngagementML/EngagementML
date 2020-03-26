9f7b4b19a494e68400f892b2010c79560c09c32c
Gustavo Rivera Pecunia <grpecunia@gmail.com>
2020-03-25 20:30:28 -0400

    changes to the home frontend

--- frontend/src/components/home/HomeComponents/Home.jsx
+++ frontend/src/components/home/HomeComponents/Home.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import logo from '../../../images/engagementML.png'
 import { Link } from 'react-router-dom'
+import axios from "axios"
 
   
 class HomeComp extends Component {
+  
+  
+
+  
 
   render() {
     return (
@@ -22,7 +27,8 @@ class HomeComp extends Component {
               </header>
               <p>
                 Welcome to <strong>EngagementML</strong> a machine learning
-                applications developed for social media management research and strategy
+                applications developed for social media management research and
+                strategy
               </p>
               <footer>
                 <a
@@ -48,7 +54,8 @@ class HomeComp extends Component {
               <p>
                 This application lets its user analyze the data from the last 12
                 IG posts and runs our complex machine learning algorythm using
-                regression analysis to reccomend optimizations which will increase your engagement rate.
+                regression analysis to reccomend optimizations which will
+                increase your engagement rate.
               </p>
               <br />
               <br />
@@ -111,7 +118,7 @@ class HomeComp extends Component {
                     <img
                       src="https://scontent-mia3-2.cdninstagram.com/v/t51.2885-19/s320x320/78964845_582853779208367_1558803722329391104_n.jpg?_nc_ht=scontent-mia3-2.cdninstagram.com&_nc_ohc=MaFcSJEOyz8AX-Ck7bI&oh=0206e796d9fcca04c441ffba6be22aa3&oe=5EA47B7C"
                       title="Drake"
-                      alt=""
+                      alt="Drake"
                     />
                   </a>
                 </article>
@@ -123,7 +130,7 @@ class HomeComp extends Component {
                     <img
                       src="https://scontent-mia3-2.cdninstagram.com/v/t51.2885-19/s320x320/71594249_1125833014288751_8012044229767331840_n.jpg?_nc_ht=scontent-mia3-2.cdninstagram.com&_nc_ohc=5Y-RG_D12YMAX92Inoy&oh=7568e195b25d372e12db1c7542063d03&oe=5EA20F3A"
                       title="Jennifer Aniston"
-                      alt=""
+                      alt="Jennifer Aniston"
                     />
                   </a>
                 </article>
@@ -135,7 +142,7 @@ class HomeComp extends Component {
                     <img
                       src="https://scontent-mia3-2.cdninstagram.com/v/t51.2885-19/s320x320/87485911_2595250347427294_7578127957344911360_n.jpg?_nc_ht=scontent-mia3-2.cdninstagram.com&_nc_ohc=dX6me_iuGlAAX-EOeE3&oh=f849a55d014fe72a797b47e64d244192&oe=5EA3CDEF"
                       title="Shakira"
-                      alt=""
+                      alt="Shakira"
                     />
                   </a>
                 </article>
@@ -147,7 +154,7 @@ class HomeComp extends Component {
                     <img
                       src="https://scontent-mia3-2.cdninstagram.com/v/t51.2885-19/s320x320/71718204_448894825985782_2989796854381674496_n.jpg?_nc_ht=scontent-mia3-2.cdninstagram.com&_nc_ohc=6hPWKuGMvGwAX-HkJOy&oh=55da1033e98cce0bb1a3a8912a7a2865&oe=5E9F2CA9"
                       title="Billie Eilish"
-                      alt=""
+                      alt="Billie Eilish"
                     />
                   </a>
                 </article>
@@ -159,7 +166,7 @@ class HomeComp extends Component {
                     <img
                       src="https://scontent-mia3-2.cdninstagram.com/v/t51.2885-19/s320x320/72873456_425339975028135_3038321055589466112_n.jpg?_nc_ht=scontent-mia3-2.cdninstagram.com&_nc_ohc=MKkZivG9pn8AX-XN7_P&oh=a0e437709b7e20de627957a616131007&oe=5E9C9FA3"
                       title="Priyanka Chopra"
-                      alt=""
+                      alt="Priyanka Chopra"
                     />
                   </a>
                 </article>
@@ -171,7 +178,7 @@ class HomeComp extends Component {
                     <img
                       src="https://scontent-mia3-2.cdninstagram.com/v/t51.2885-19/s320x320/60069326_2844010522491121_8257963186574065664_n.jpg?_nc_ht=scontent-mia3-2.cdninstagram.com&_nc_ohc=tzw6nxWj6JIAX_uKbbu&oh=df8754150a8b8a54b71d1984dee08085&oe=5EA45B85"
                       title="James Rodriguez"
-                      alt=""
+                      alt="James Rodriguez"
                     />
                   </a>
                 </article>
