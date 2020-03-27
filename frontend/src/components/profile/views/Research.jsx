8e92bcfd980d6900fb15ac255de4f9448fc68337
Gustavo Rivera Pecunia <grpecunia@gmail.com>
2020-03-26 21:11:13 -0400

    changes and updates

--- frontend/src/components/profile/views/Research.jsx
+++ frontend/src/components/profile/views/Research.jsx
@@ -1,15 +1,73 @@
 
 import React, { Component } from "react";
 import { Container, Row, Col, Carousel } from "react-bootstrap";
-// import axios from "axios"
+import axios from "axios";
+import actions from "../../../services/index";
 import Card from "../components/Card/Card.jsx";
 
 // axios.get("http://localhost:5000/profiles").then(res => console.log(res.data));
 
 class Research extends Component {
+  state = {};
+
+  async componentDidMount() {
+    let user = await actions.isLoggedIn();
+    this.setState({ ...user.data });
+    // console.log("Current User >> ", user);
+
+    await axios
+      // .get("http://localhost:5000/profile/" + this.state.igUsername)
+      .get(
+        "https://engagementml.herokuapp.com/profile/" + this.state.igUsername
+      )
+      .then(res => {
+        // console.log(res, res.data);
+        this.setState({
+          profile: res.data
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+
+    await axios
+      .get("https://engagementml.herokuapp.com/posts/" + this.state.profile.id)
+      .then(res => {
+        // console.log(res, res.data);
+        this.setState({
+          posts: res.data
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+
+    await axios
+      // .get("http://localhost:5000/eML/user/" + this.props.match.params.id)
+      .get(
+        "https://engagementml.herokuapp.com/eML/user/" +
+          this.props.match.params.id
+      )
+      .then(res => {
+        this.setState({
+          email: res.data.email,
+          name: res.data.name,
+          fname: res.data.fname,
+          lname: res.data.lname,
+          igUsername: res.data.igUsername,
+          image: res.data.image,
+          about: res.data.about,
+          industry: res.data.industry
+          // role: res.data.role,
+          // competitor: res.data.competitor,
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
 
   render() {
-  
     return (
       <div className="content" style={{ height: "100vh" }}>
         <Container fluid="true">
@@ -23,13 +81,13 @@ class Research extends Component {
               // key={i}
             >
               <Card
-                title="General Post Recomendations"
-                category="eML Recommendations"
+                title="Followers vs Comments Count"
+                category="eML Comparative Analysis"
                 content={
                   <img
                     className="d-block alignItemsCenter"
-                    src="https://egagementml.s3.amazonaws.com/post+general+recommendations.jpg"
-                    alt="Followers Vs Likes"
+                    src={`https://engagementmlapp.s3.amazonaws.com/${this.state.profile.industry}/Followers_VS_Comments_Counts.jpeg`}
+                    alt="Followers vs Comments Count"
                     height="105"
                   />
                 }
@@ -44,12 +102,12 @@ class Research extends Component {
               // key={i}
             >
               <Card
-                title="Daily Post Optimizations"
-                category="eML Recommendations"
+                title="Followers vs Likes"
+                category="eML Comparative Analysis"
                 content={
                   <img
                     className="d-block alignItemsCenter"
-                    src="https://egagementml.s3.amazonaws.com/post+per+day+recommendation.jpg"
+                    src={`https://engagementmlapp.s3.amazonaws.com/${this.state.profile.industry}/Followers_VS_Likes.jpeg`}
                     alt="Followers Vs Likes"
                     width="270"
                   />
@@ -65,13 +123,13 @@ class Research extends Component {
               // key={i}
             >
               <Card
-                title="Weekly Post Optimizations"
-                category="eML Recommendations"
+                title="Followers vs Video View Count"
+                category="eML Comparative Analysis"
                 content={
                   <img
                     className="d-block alignItemsCenter"
-                    src="https://egagementml.s3.amazonaws.com/post+per+hour+recommendation.jpg"
-                    alt="Followers Vs Likes"
+                    src={`https://engagementmlapp.s3.amazonaws.com/${this.state.profile.industry}/Followers_VS_Video_view_Count.jpeg`}
+                    alt="Followers vs Video View Count"
                     width="270"
                   />
                 }
@@ -91,7 +149,7 @@ class Research extends Component {
                 content={
                   <img
                     className="d-block alignItemsCenter"
-                    src="https://egagementml.s3.us-east-1.amazonaws.com/Correlation%20Matrix.png"
+                    src={`https://engagementmlapp.s3.amazonaws.com/${this.state.profile.industry}/Correlation_Matrix.jpeg`}
                     alt="Correlation"
                     width="400"
                   />
@@ -112,7 +170,7 @@ class Research extends Component {
                 content={
                   <img
                     className="d-block alignItemsCenter"
-                    src="https://egagementml.s3.amazonaws.com/wordcloud+food+.png"
+                    src={`https://engagementmlapp.s3.amazonaws.com/${this.state.profile.industry}/word_cloud.jpeg`}
                     alt="Word Cloud"
                     width="350"
                   />
@@ -128,13 +186,13 @@ class Research extends Component {
               // key={i}
             >
               <Card
-                title="Followers vs Video Views"
+                title="Link Count vs Like Count"
                 category="eML Comparative Analysis"
                 content={
                   <img
                     className="d-block alignItemsCenter"
-                    src="https://egagementml.s3.us-east-1.amazonaws.com/Followers%20VS%20Video%20View%20Counts.png"
-                    alt="Followers Vs Likes"
+                    src={`https://engagementmlapp.s3.amazonaws.com/${this.state.profile.industry}/Link_Count_VS_Like_Count.jpeg`}
+                    alt="Link Count vs Like Count"
                     width="400"
                   />
                 }
@@ -149,13 +207,13 @@ class Research extends Component {
               // key={i}
             >
               <Card
-                title="Followers Vs Likes"
+                title="Text Length vs Hashtag Count"
                 category="eML Comparative Analysis"
                 content={
                   <img
                     className="d-block alignItemsCenter"
-                    src="https://egagementml.s3.us-east-1.amazonaws.com/Followers%20Vs%20Likes.png"
-                    alt="Followers Vs Likes"
+                    src={`https://engagementmlapp.s3.amazonaws.com/${this.state.profile.industry}/Text_Lenght_VS_Hashtag_Count.jpeg`}
+                    alt="Text Length vs Hashtag Count"
                     width="400"
                   />
                 }
@@ -170,13 +228,13 @@ class Research extends Component {
               // key={i}
             >
               <Card
-                title="Followers vs Comment Counts"
+                title="Text Length vs Like Count"
                 category="eML Comparative Analysis"
                 content={
                   <img
                     className="d-block alignItemsCenter"
-                    src="https://egagementml.s3.us-east-1.amazonaws.com/Follwers%20VS%20Comment%20Counts.png"
-                    alt="Followers vs Comment Counts"
+                    src={`https://engagementmlapp.s3.amazonaws.com/${this.state.profile.industry}/Text_Length_Vs_Like_Count.jpeg`}
+                    alt="Text Length vs Like Count"
                     width="400"
                   />
                 }
@@ -191,13 +249,13 @@ class Research extends Component {
               // key={i}
             >
               <Card
-                title="Text vs Hashtag"
+                title="Followers vs Likes by Business Category"
                 category="eML Comparative Analysis"
                 content={
                   <img
                     className="d-block alignItemsCenter"
-                    src="https://egagementml.s3.amazonaws.com/Text+Length+VS+Hashtag+Count.png"
-                    alt="Text vs Hashtag"
+                    src={`https://engagementmlapp.s3.amazonaws.com/${this.state.profile.industry}/Followers_Vs_Like_Count_-_Segmented_by_Business_Categories.jpeg`}
+                    alt="Followers vs Likes by Business Category"
                     width="400"
                   />
                 }
@@ -207,7 +265,7 @@ class Research extends Component {
         </Container>
       </div>
     );
-        }
-      }
+  }
+}
 
 export default Research;
