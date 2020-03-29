00cbdd2412986a45a09dd35b14863bd56d34b0f6
Gustavo Rivera Pecunia <grpecunia@gmail.com>
2020-03-29 02:31:01 -0400

    changes to url fix check

--- frontend/src/components/home/HomeComponents/Home.jsx
+++ frontend/src/components/home/HomeComponents/Home.jsx
@@ -12,7 +12,7 @@ class HomeComp extends Component {
     let message = document.getElementById("message").value;
     axios({
       method: "POST",
-      url: "https://engagementml.herokuapp.com/send",
+      url: "http://localhost:5000/send",
       data: {
         name: name,
         email: email,
