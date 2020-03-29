50c980c26183de704b57b3204e77215d9aea83a0
Gustavo Rivera Pecunia <grpecunia@gmail.com>
2020-03-28 23:35:26 -0400

    pushing alerts for login and signup error handling

--- frontend/src/components/auth/LogIn.js
+++ frontend/src/components/auth/LogIn.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import actions from '../../services/index'
 import logo from "../../images/engagementMLsm.png";
-import { Button } from 'react-bootstrap'
+import { Button, Modal } from 'react-bootstrap'
 import IGbackBlue from '../../images/backgroundBlue.png'
 // import { Link } from 'react-router-dom';
 import { FaKey, FaUser } from "react-icons/fa";
 
 class LogIn extends Component {
-  state = {};
+  state = {
+    show: false, 
+  };
   handleChange = e => this.setState({ [e.target.name]: e.target.value });
 
   handleSubmit = e => {
@@ -15,86 +17,115 @@ class LogIn extends Component {
     actions
       .logIn(this.state)
       .then(user => {
-        this.props.setUser({ ...user.data })
+        this.props.setUser({ ...user.data });
         // console.log(this.props)
-        this.props.history.push('/profile/admin/dashboard')
+        this.props.history.push("/profile/admin/dashboard");
       })
-      .catch(({ response }) => console.error(response.data));
+      .catch(({ response }) => {
+        console.log("before >>>");
+        this.setState({
+          show: true
+        });
+        console.error(response.data);
+      });
   };
 
-  
+  showAlert = () => {
+    this.setState({ show: true });
+  };
+
+  hideAlert = () => {
+    this.setState({ show: false });
+  };
 
   render() {
-
     return (
-      <div
-        style={{
-          backgroundImage: `url(${IGbackBlue})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          height: "100vh"
-        }}
-      >
-        <div className="container h-100">
-          <div className="d-flex justify-content-center h-100">
-            <div className="d-flex flex-column justify-content-center">
-              <div className="d-flex justify-content-center">
-                <div className="brand_logo_container">
-                  <img
-                    src={logo}
-                    width="100%"
-                    className="brand_logo"
-                    alt="Logo"
-                  />
+      <>
+        <Modal
+          size="sm"
+          show={this.state.show}
+          onHide={() => this.hideAlert()}
+          aria-labelledby="example-modal-sizes-title-sm"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="example-modal-sizes-title-sm">
+              <strong>Error on Login!</strong>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            There is an error with your username / password combination. Please
+            check to make sure your using the correct username / password
+            combination.
+          </Modal.Body>
+        </Modal>
+        <div
+          style={{
+            backgroundImage: `url(${IGbackBlue})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            height: "100vh"
+          }}
+        >
+          <div className="container h-100">
+            <div className="d-flex justify-content-center h-100">
+              <div className="d-flex flex-column justify-content-center">
+                <div className="d-flex justify-content-center">
+                  <div className="brand_logo_container">
+                    <img
+                      src={logo}
+                      width="100%"
+                      className="brand_logo"
+                      alt="Logo"
+                    />
+                  </div>
                 </div>
-              </div>
-              <div
-                className="d-flex justify-content-center form_container"
-                id="signUpCopy"
-              >
-                <form onSubmit={this.handleSubmit}>
-                  <div className="input-group mb-3">
-                    <div className="input-group-append">
-                      <span className="input-group-text">
-                        <FaUser />
-                      </span>
+                <div
+                  className="d-flex justify-content-center form_container"
+                  id="signUpCopy"
+                >
+                  <form onSubmit={this.handleSubmit}>
+                    <div className="input-group mb-3">
+                      <div className="input-group-append">
+                        <span className="input-group-text">
+                          <FaUser />
+                        </span>
+                      </div>
+                      <input
+                        placeholder="Email here "
+                        style={{
+                          border: "0",
+                          outline: "0",
+                          background: "transparent",
+                          borderBottom: "0.15rem solid #e5e6e7",
+                          borderRadius: "0"
+                        }}
+                        name="email"
+                        type="email"
+                        onChange={this.handleChange}
+                      />
                     </div>
-                    <input
-                      placeholder="Email here "
-                      style={{
-                        border: "0",
-                        outline: "0",
-                        background: "transparent",
-                        borderBottom: "0.15rem solid #e5e6e7",
-                        borderRadius: "0"
-                      }}
-                      name="email"
-                      type="email"
-                      onChange={this.handleChange}
-                    />
-                  </div>
-                  <div className="input-group mb-2">
-                    <div className="input-group-append">
-                      <span className="input-group-text">
-                        <FaKey />
-                      </span>
+                    <div className="input-group mb-2">
+                      <div className="input-group-append">
+                        <span className="input-group-text">
+                          <FaKey />
+                        </span>
+                      </div>
+                      <input
+                        placeholder="Password here "
+                        style={{
+                          border: "0",
+                          outline: "0",
+                          background: "transparent",
+                          borderBottom: "0.15rem solid #e5e6e7",
+                          borderRadius: "0"
+                        }}
+                        name="password"
+                        type="password"
+                        onChange={this.handleChange}
+                      />
                     </div>
-                    <input
-                      placeholder="Password here "
-                      style={{
-                        border: "0",
-                        outline: "0",
-                        background: "transparent",
-                        borderBottom: "0.15rem solid #e5e6e7",
-                        borderRadius: "0"
-                      }}
-                      name="password"
-                      type="password"
-                      onChange={this.handleChange}
-                    />
-                  </div>
-                  {/* This below is the remember me checkbox */}
-                  {/* <div className="form-group mt-4">
+                    {/* This below is the remember me checkbox */}
+                    {/* <div className="form-group mt-4">
                     <div className="custom-control custom-checkbox" style={{border: "0",outline: "0", background: "transparent", borderBottom: '0.15rem solid #e5e6e7', borderRadius:"0",fontFamily: `Courier New, Courier, monospace`,color: 'whitesmoke', opacity: '0.5' }}>
                       <input
                         type="checkbox"
@@ -109,41 +140,42 @@ class LogIn extends Component {
                       </label>
                     </div>
                   </div> */}
-                  <div className="d-flex justify-content-center mt-5 login_container">
-                    <Button
-                      type="submit"
-                      value="Log In"
-                      style={{ borderColor: "#e5e6e7", color: "#e5e6e7" }}
-                    >
-                      Log In
-                    </Button>
-                  </div>
-                </form>
-              </div>
-              <div className="mt-4">
-                <div
-                  className="d-flex justify-content-center links"
-                  style={{ color: "whitesmoke", opacity: "0.5" }}
-                >
-                  Don't have an account?{" "}
-                  <a
-                    href="/sign-up"
-                    className="ml-2"
-                    style={{ textDecoration: "underline" }}
-                  >
-                    Sign Up
-                  </a>
+                    <div className="d-flex justify-content-center mt-5 login_container">
+                      <Button
+                        type="submit"
+                        value="Log In"
+                        style={{ borderColor: "#e5e6e7", color: "#e5e6e7" }}
+                      >
+                        Log In
+                      </Button>
+                    </div>
+                  </form>
                 </div>
-                <div className="d-flex justify-content-center links">
-                  <a href="mailto:engagementml@gmail.com?subject=Forgot%20my%20eML%20Password">
-                    Forgot your password?
-                  </a>
+                <div className="mt-4">
+                  <div
+                    className="d-flex justify-content-center links"
+                    style={{ color: "whitesmoke", opacity: "0.5" }}
+                  >
+                    Don't have an account?{" "}
+                    <a
+                      href="/sign-up"
+                      className="ml-2"
+                      style={{ textDecoration: "underline" }}
+                    >
+                      Sign Up
+                    </a>
+                  </div>
+                  <div className="d-flex justify-content-center links">
+                    <a href="mailto:engagementml@gmail.com?subject=Forgot%20my%20eML%20Password">
+                      Forgot your password?
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
--- frontend/src/components/auth/SignUp.js
+++ frontend/src/components/auth/SignUp.js
@@ -3,14 +3,15 @@ import actions from '../../services/index'
 import logo from "../../images/engagementMLsm.png";
 import IGback from '../../images/igback.jpg'
 // frontend\src\images\instagram-background.jpg
-import { Button } from "react-bootstrap";
+import { Button, Modal } from "react-bootstrap";
 import { FaKey, FaUser } from "react-icons/fa";
 // import waitForSingleData from "../../../../backend/routes/singleInstaRoute/SingleRoute"
 // import { Link } from "react-router-dom";
 
 class SignUp extends Component {
     state = {
-
+      show:false,
+      error: ''
     } 
     handleChange = e => this.setState({[e.target.name]: e.target.value})
 
@@ -22,78 +23,126 @@ class SignUp extends Component {
                 this.props.history.push("/profile/admin/user")
                  
             }).catch(({ response }) => {
-                       console.error(response.data);
+                      // console.log("before >>>")
+                      this.setState({
+                        show:true,
+                        error: response.data.err.message
+                      })
+                      //  console.error(response.data);
+                       console.log(response.data.err.message)
+
                      })
     }
+
+    showAlert = () => {
+      this.setState({show:true})
+    }
+
+    hideAlert = () => {
+      this.setState({show:false})
+    }
+  
  
     render() {
        
         return (
-          <div
-            style={{
-              backgroundImage: `url(${IGback})`,
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "cover",
-              height: "100vh"
-            }}
-          >
-            <div className="container h-100">
-              <div className="d-flex justify-content-center h-100">
-                <div className="d-flex flex-column justify-content-center">
-                  <div className="d-flex justify-content-center">
-                    <div className="brand_logo_container">
-                      <img
-                        src={logo}
-                        width="100%"
-                        className="brand_logo"
-                        alt="Logo"
-                      />
-                      <p style={{ textAlign: "center", color: "white", fontFamily: `'Courier New', Courier, monospace`}}>
-                        <strong  style={{fontFamily: `'Courier New', Courier, monospace`}}>
-                          Please read before signing up!
-                          <br />
-                        </strong >
-                        In order to use the EngagementML application your
-                        Instagram account{" "}
-                        <strong style={{fontFamily: `'Courier New', Courier, monospace`}}>
-                          MUST BE PUBLIC. <br />
-                          <span
-                            className="loading"
-                            style={{color: "white",fontFamily: `'Courier New', Courier, monospace` }}
-                          >
-                            {" "}
-                            ** If your account is private you WILL NOT BE ABLE
-                            to access our platform! **{" "}
-                          </span>
-                        </strong>
-                      </p>
-                    </div>
-                  </div>
-                  <div
-                    className="d-flex justify-content-center form_container"
-                    id="signUpCopy"
-                  >
-                    <form onSubmit={this.handleSubmit}>
-                      <div className="input-group mb-3">
-                        <div className="input-group-append">
-                          <span className="input-group-text">
-                            <FaUser />
-                          </span>
-                        </div>
-                        <input
-                          placeholder="Email here"
-                          style={{
-                            border: "0",
-                            outline: "0",
-                            background: "transparent",
-                            borderBottom: "0.15rem solid #e5e6e7",
-                            borderRadius: "0"
-                          }}
-                          name="email"
-                          type="email"
-                          onChange={this.handleChange}
+          <>
+            <Modal
+              size="sm"
+              show={this.state.show}
+              onHide={() => this.hideAlert()}
+              aria-labelledby="example-modal-sizes-title-sm"
+            >
+              <Modal.Header closeButton>
+                <Modal.Title id="example-modal-sizes-title-sm">
+                  <strong>Error on Sign-Up!</strong>
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>{this.state.error}</Modal.Body>
+            </Modal>
+            <div
+              style={{
+                backgroundImage: `url(${IGback})`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                height: "100vh"
+              }}
+            >
+              <div className="container h-100">
+                <div className="d-flex justify-content-center h-100">
+                  <div className="d-flex flex-column justify-content-center">
+                    <div className="d-flex justify-content-center">
+                      <div className="brand_logo_container">
+                        <img
+                          src={logo}
+                          width="100%"
+                          className="brand_logo"
+                          alt="Logo"
                         />
-                        {/* <input
+
+                        <p
+                          style={{
+                            textAlign: "center",
+                            color: "white",
+                            fontFamily: `'Courier New', Courier, monospace`
+                          }}
+                        >
+                          <strong
+                            style={{
+                              fontFamily: `'Courier New', Courier, monospace`
+                            }}
+                          >
+                            Please read before signing up!
+                            <br />
+                          </strong>
+                          In order to use the EngagementML application your
+                          Instagram account{" "}
+                          <strong
+                            style={{
+                              fontFamily: `'Courier New', Courier, monospace`
+                            }}
+                          >
+                            MUST BE PUBLIC. <br />
+                            <span
+                              className="loading"
+                              style={{
+                                color: "white",
+                                fontFamily: `'Courier New', Courier, monospace`
+                              }}
+                            >
+                              {" "}
+                              ** If your account is private you WILL NOT BE ABLE
+                              to access our platform! **{" "}
+                            </span>
+                          </strong>
+                        </p>
+                      </div>
+                    </div>
+                    <div
+                      className="d-flex justify-content-center form_container"
+                      id="signUpCopy"
+                    >
+                      <form onSubmit={this.handleSubmit}>
+                        <div className="input-group mb-3">
+                          <div className="input-group-append">
+                            <span className="input-group-text">
+                              <FaUser />
+                            </span>
+                          </div>
+                          <input
+                            placeholder="Email here"
+                            style={{
+                              border: "0",
+                              outline: "0",
+                              background: "transparent",
+                              borderBottom: "0.15rem solid #e5e6e7",
+                              borderRadius: "0"
+                            }}
+                            name="email"
+                            type="email"
+                            onChange={this.handleChange}
+                          />
+                          {/* <input
                           placeholder="IG Username"
                           style={{
                             border: "0",
@@ -106,64 +155,65 @@ class SignUp extends Component {
                           type="text"
                           onChange={this.handleChange}
                         /> */}
-                      </div>
-                      <div className="input-group mb-3">
-                        <div className="input-group-append">
-                          <span className="input-group-text">
-                            <FaKey />
-                          </span>
                         </div>
-                        <input
-                          placeholder="Password here "
-                          style={{
-                            border: "0",
-                            outline: "0",
-                            background: "transparent",
-                            borderBottom: "0.15rem solid #e5e6e7",
-                            borderRadius: "0"
-                          }}
-                          name="password"
-                          type="password"
-                          onChange={this.handleChange}
-                        />
-                      </div>
-                      <div className="input-group mb-3">
-                        <div className="input-group-append">
-                          <span className="input-group-text">
-                            <FaUser />
-                          </span>
+                        <div className="input-group mb-3">
+                          <div className="input-group-append">
+                            <span className="input-group-text">
+                              <FaKey />
+                            </span>
+                          </div>
+                          <input
+                            placeholder="Password here "
+                            style={{
+                              border: "0",
+                              outline: "0",
+                              background: "transparent",
+                              borderBottom: "0.15rem solid #e5e6e7",
+                              borderRadius: "0"
+                            }}
+                            name="password"
+                            type="password"
+                            onChange={this.handleChange}
+                          />
                         </div>
+                        <div className="input-group mb-3">
+                          <div className="input-group-append">
+                            <span className="input-group-text">
+                              <FaUser />
+                            </span>
+                          </div>
 
-                        <input
-                          placeholder="IG Username"
-                          style={{
-                            border: "0",
-                            outline: "0",
-                            background: "transparent",
-                            borderBottom: "0.15rem solid #e5e6e7",
-                            borderRadius: "0"
-                          }}
-                          name="igUsername"
-                          type="text"
-                          onChange={this.handleChange}
-                        />
-                      </div>
-                      <div className="d-flex justify-content-center mt-5 login_container">
-                        {/* <Button as={Link} to="/profile/admin/dashboard" type="submit" value="Sign Up" style={{borderColor:'#e5e6e7',color:"#e5e6e7"}}> */}
-                        <Button
-                          type="submit"
-                          value="Sign Up"
-                          style={{ borderColor: "#e5e6e7", color: "#e5e6e7" }}
-                        >
-                          Sign Up
-                        </Button>
-                      </div>
-                    </form>
+                          <input
+                            placeholder="IG Username"
+                            style={{
+                              border: "0",
+                              outline: "0",
+                              background: "transparent",
+                              borderBottom: "0.15rem solid #e5e6e7",
+                              borderRadius: "0"
+                            }}
+                            name="igUsername"
+                            type="text"
+                            onChange={this.handleChange}
+                          />
+                        </div>
+                        <div className="d-flex justify-content-center mt-5 login_container">
+                          {/* <Button as={Link} to="/profile/admin/dashboard" type="submit" value="Sign Up" style={{borderColor:'#e5e6e7',color:"#e5e6e7"}}> */}
+                          <Button
+                            type="submit"
+                            value="Sign Up"
+                            style={{ borderColor: "#e5e6e7", color: "#e5e6e7" }}
+                          >
+                            Sign Up
+                          </Button>
+                        </div>
+                      </form>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          </>
         );
     }
 }
